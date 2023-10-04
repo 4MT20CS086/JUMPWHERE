06990e59725d3c5dbd594588177af07e98264649
KIRANKUMAR K MADAR <96112562+4MT20CS086@users.noreply.github.com>
2023-10-04 18:59:33 +0530

    Add files via upload

--- assigment 8/5.py
+++ assigment 8/5.py
@@ -1,1 +1,11 @@
-
+class ThreeSum:
+    def _init_(self, numbers):
+        self.numbers = numbers
+    def find_triplets(self):
+        triplets = []
+        for i in range(len(self.numbers)):
+            for j in range(i + 1, len(self.numbers)):
+                for k in range(j + 1, len(self.numbers)):
+                    if self.numbers[i] + self.numbers[j] + self.numbers[k] == 0:
+                        triplets.append([self.numbers[i], self.numbers[j], self.numbers[k]])
+        return triplets
