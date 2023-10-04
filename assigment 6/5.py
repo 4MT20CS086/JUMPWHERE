2934d70040c8db8e778420bcecdbadc739a45241
KIRANKUMAR K MADAR <96112562+4MT20CS086@users.noreply.github.com>
2023-10-04 12:43:24 +0530

    Add files via upload

--- assigment 6/5.py
+++ assigment 6/5.py
@@ -1,1 +1,17 @@
-
+def main():
+    n = 0
+    sum = 0
+    while True:
+        num = int(input("Enter an integer number (0 to finish): "))
+        if num == 0:
+            break
+        n += 1
+        sum += num
+    if n == 0:
+        print("No numbers entered.")
+    else:
+        average = sum / n
+        print(f"Sum of the numbers: {sum}")
+        print(f"Average of the numbers: {average}")
+if _name_ == "_main_":
+    main()
