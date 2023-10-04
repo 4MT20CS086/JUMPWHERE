c4194a2b455fd0f09721863ac3b64e0187a43407
KIRANKUMAR K MADAR <96112562+4MT20CS086@users.noreply.github.com>
2023-10-04 19:04:14 +0530

    Add files via upload

--- assigment 8/10.py
+++ assigment 8/10.py
@@ -1,1 +1,7 @@
-
+ class MyClass:
+    def _init_(self, name):
+        self.name = name
+    def get_class_name(self):
+        return self._class.name_
+my_instance = MyClass("MyInstance")
+print(my_instance.get_class_name())
