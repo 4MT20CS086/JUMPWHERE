db57e093b0aa0eed77fa51468bb2a6f6dfe95a12
KIRANKUMAR K MADAR <96112562+4MT20CS086@users.noreply.github.com>
2023-10-04 19:00:24 +0530

    Add files via upload

--- assigment 8/6.py
+++ assigment 8/6.py
@@ -1,1 +1,17 @@
-
+class Power:
+    def _init_(self, x, n):
+        self.x = x
+        self.n = n
+    def pow(self):
+        if self.n == 0:
+            return 1
+        elif self.n < 0:
+            return 1 / self.pow(self.x, -self.n)
+        else:
+            result = 1
+            while self.n > 0:
+                if self.n % 2 == 1:
+                    result *= self.x
+                self.x *= self.x
+                self.n //= 2
+            return result
