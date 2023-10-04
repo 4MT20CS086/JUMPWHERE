fd6c111c16b5a5e9aefc03d3cfe9818a61264209
KIRANKUMAR K MADAR <96112562+4MT20CS086@users.noreply.github.com>
2023-10-04 19:01:28 +0530

    Add files via upload

--- assigment 8/7.py
+++ assigment 8/7.py
@@ -1,1 +1,7 @@
-
+class ReverseString:
+    def _init_(self, string):
+        self.string = string
+    def reverse_words(self):
+        words = self.string.split()
+        words.reverse()
+        return ' '.join(words
